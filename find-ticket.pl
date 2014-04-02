914d8a9669363f3e2fc8be69561a016ff4c260ea
Paul Waring <paul.waring@manchester.ac.uk>
2014-04-02 15:28:26 +0100

    Create ticket object
    Use example from documentation in Kernel/System/Ticket.pm

--- find-ticket.pl
+++ find-ticket.pl
@@ -4,16 +4,44 @@ use strict;
 use warnings;
 
 use lib '/var/lib/otrs/otrs-3.2.10';
-use Kernel::System::TicketSearch;
 
-my $ticket_id = $ARGV[0];
+use Kernel::Config;
+use Kernel::System::Encode;
+use Kernel::System::Log;
+use Kernel::System::Time;
+use Kernel::System::Main;
+use Kernel::System::DB;
+use Kernel::System::Ticket;
 
-my %query = (
-  Result => 'ARRAY',
-  TicketID => $ticket_id,
+my $ConfigObject = Kernel::Config->new();
+my $EncodeObject = Kernel::System::Encode->new(
+  ConfigObject => $ConfigObject,
+);
+my $LogObject = Kernel::System::Log->new(
+  ConfigObject => $ConfigObject,
+  EncodeObject => $EncodeObject,
+);
+my $TimeObject = Kernel::System::Time->new(
+  ConfigObject => $ConfigObject,
+  LogObject    => $LogObject,
+);
+my $MainObject = Kernel::System::Main->new(
+  ConfigObject => $ConfigObject,
+  EncodeObject => $EncodeObject,
+  LogObject    => $LogObject,
+);
+my $DBObject = Kernel::System::DB->new(
+  ConfigObject => $ConfigObject,
+  EncodeObject => $EncodeObject,
+  LogObject    => $LogObject,
+  MainObject   => $MainObject,
+);
+my $TicketObject = Kernel::System::Ticket->new(
+  ConfigObject       => $ConfigObject,
+  LogObject          => $LogObject,
+  DBObject           => $DBObject,
+  MainObject         => $MainObject,
+  TimeObject         => $TimeObject,
+  EncodeObject       => $EncodeObject,
 );
 
-my $otrs = Kernel::System::TicketSearch->new();
-my @results = $otrs->TicketSearch(%query);
-
-print @results . "\n";
