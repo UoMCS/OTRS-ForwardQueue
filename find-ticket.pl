d1b4fbe578b30f15059524ebafa7f79ef2c88fa5
Paul Waring <paul.waring@manchester.ac.uk>
2014-04-02 15:04:00 +0100

    Simple 'find ticket' script

---
+++ find-ticket.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib '/var/lib/otrs/otrs-3.2.10';
+use Kernel::System::TicketSearch;
+
+my $ticket_id = $ARGV[1];
+my %query = (
+  Result => 'ARRAY',
+  TicketID => $ticket_id,
+);
+
+my @results = TicketSearch(%query);
+
+print @results . "\n";
