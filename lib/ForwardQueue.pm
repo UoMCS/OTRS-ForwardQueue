14630430bc7db77ca8b0c873583acee1425e025e
Paul Waring <paul.waring@manchester.ac.uk>
2014-04-04 12:27:06 +0100

    OTRS modules import

--- lib/ForwardQueue.pm
+++ lib/ForwardQueue.pm
@@ -4,7 +4,13 @@ use strict;
 use warnings;
 
 use lib '/var/lib/otrs/otrs-3.2.10';
-use Kernel::System::TicketSearch;
+use Kernel::Config;
+use Kernel::System::Encode;
+use Kernel::System::Log;
+use Kernel::System::Time;
+use Kernel::System::Main;
+use Kernel::System::DB;
+use Kernel::System::Ticket;
 
 our $VERSION = 0.01;
 
