db5b4a44e4d8427ee6d0ae5832a0d241dced1d59
Paul Waring <paul.waring@manchester.ac.uk>
2014-04-04 16:00:59 +0100

    Improvements to environment script

--- set-env.sh
+++ set-env.sh
@@ -2,10 +2,18 @@
 
 # Set environment variables - required if running from cron
 
-# If you are using local::lib
+# Comment out if you are not using local::lib
 [ $SHLVL -eq 1 ] && eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)"
 
+# Define your OTRS library path here
+OTRS_LIB=""
+EXTRALIBS="./lib:$OTRS_LIB"
+
 # Add OTRS search libraries to the library path
-PERL5LIB+="/var/lib/otrs"
+if [ -n "$PERL5LIB" ]; then
+  PERL5LIB+=":"
+fi
+
+PERL5LIB+="$EXTRALIBS"
 
 # Call your Perl script here
