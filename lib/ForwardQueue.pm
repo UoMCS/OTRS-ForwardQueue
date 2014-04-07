4b8419ad5fb8ea0b39a687c3905c81c1f3491a21
Paul Waring <paul.waring@manchester.ac.uk>
2014-04-07 15:07:23 +0100

    Synopsis documentation

--- lib/ForwardQueue.pm
+++ lib/ForwardQueue.pm
@@ -189,6 +189,26 @@ This document describes ForwardQueue version 0.0.1.
 
 =head1 SYNOPSIS
 
+    use ForwardQueue;
+
+    %query = (
+      Queues => ['MyQueue'],
+      States => ['new', 'open'],
+      Locks => ['unlock'],
+      UserID => 1,
+    );
+
+    %options = (
+      ForwardTo => 'nobody@example.org',
+      TempDir => '/tmp',
+      HistoryComment => 'Forward to other request system',
+      SMTP => 1,
+      SMTPServer => 'smtp.example.org',
+    );
+
+    my $fq = ForwardQueue->new('query' => \%query, 'options' => \%options);
+
+    $fp->process_queue();
 
 =head1 DESCRIPTION
 
