d6c6ac473cf208dca96d1c9dfccf52c9e5bb5f21
Paul Waring <paul.waring@manchester.ac.uk>
2014-04-16 15:35:38 +0100

    Bump version number

--- lib/OTRS/ForwardQueue.pm
+++ lib/OTRS/ForwardQueue.pm
@@ -24,7 +24,7 @@ use Kernel::System::DB;
 use Kernel::System::Ticket;
 use Kernel::System::Ticket::Article;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 has 'query' => (
   traits => ['Hash'],
@@ -237,7 +237,7 @@ OTRS::ForwardQueue - Forwards the contents of an OTRS queue to a given email add
 
 =head1 VERSION
 
-This document describes OTRS::ForwardQueue version 0.0.1.
+This document describes OTRS::ForwardQueue version 0.0.2.
 
 =head1 SYNOPSIS
 
