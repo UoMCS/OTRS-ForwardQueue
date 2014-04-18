6b89cd54bc401713833bf104f4e196075f0d6306
Paul Waring <paul@xk7.net>
2014-04-18 17:07:41 +0100

    Bump version to 0.05

--- lib/OTRS/ForwardQueue.pm
+++ lib/OTRS/ForwardQueue.pm
@@ -24,7 +24,7 @@ use Kernel::System::DB;
 use Kernel::System::Ticket;
 use Kernel::System::Ticket::Article;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 has 'query' => (
   traits => ['Hash'],
