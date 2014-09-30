9b3501899c0db02f1eddfae0629c186ae43dbb55
Paul Waring <paul.waring@manchester.ac.uk>
2014-09-30 12:54:29 +0100

    Bump to version 0.09

--- lib/OTRS/ForwardQueue.pm
+++ lib/OTRS/ForwardQueue.pm
@@ -24,7 +24,7 @@ use Kernel::System::DB;
 use Kernel::System::Ticket;
 use Kernel::System::Ticket::Article;
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 has 'query' => (
   traits => ['Hash'],
